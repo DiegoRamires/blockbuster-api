5d8ef6d5af1e61059620369d3315e1c6e73c8e91
DiegoRamires <goncalves.ramires@gmail.com>
2019-10-05 15:06:32 -0300

    install mongoose to db connection, add data persist on database. refactor endpoints to mongo valid requests

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const Joi = require('joi')
+const mongoose = require('mongoose')
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+
+mongoose.connect('mongodb://localhost/blockbuster',
+  { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB!'))
 
 const genres = require('./routes/genres')
 
